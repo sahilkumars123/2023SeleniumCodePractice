c13daeb0157f2d131c34a567540f038e478b122e
Sahil.kumar <sahil.kumar@BLR1-LMC-N71387.local>
2023-07-09 10:03:09 +0530

    added few more files

---
+++ src/main/java/GitPractice/SearchPage.java
@@ -0,0 +1,8 @@
+package GitPractice;
+
+public class SearchPage {
+
+    public static void main(String[] args) {
+        System.out.println("This is Search Page");
+    }
+}
---
+++ src/test/java/SearchPageTest.java
@@ -0,0 +1,6 @@
+public class SearchPageTest {
+
+    public static void main(String[] args) {
+        System.out.println("This is Seach Page Test");
+    }
+}
